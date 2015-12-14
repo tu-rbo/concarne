642dcf39e44d23b7a9669ef1a86029f1c32db17c
shoefer <sebastian.hoefer@tu-berlin.de>
2015-12-14 17:54:01 +0100

    added multiview test

--- tests/pattern_linear_test.py
+++ tests/pattern_linear_test.py
@@ -19,44 +19,56 @@ def build_linear_simple(input_layer, n_out, nonlinearity=None, name=None):
     return network    
 
 
-class TestDirectData(object):
+class TestBase(object):
     """
       Setup class generating some simplistic direct data
     """
+    
+    def __init__(self):
+        self.loss_weights = {'target_weight':0.9, 'context_weight':0.1}
+    
     def setup(self):
         self.X = np.array( [[0,1,2,3,4], [1,1,1,1,1]]).T
         self.S = self.X[:,:1]
         self.Y = np.array( [0, 0, 1, 1, 1], dtype='int32' )
-        self.C = np.array( [[0,1,2,3,4], ] ).T
         
         self.n = self.X.shape[1]
-        self.m = self.C.shape[1]
         
-        self.num_classes = 2
+        self.init_variables()
+        self.build_pattern()
 
-class TestDirectPattern(TestDirectData):
+    def init_variables(self):
+        self.input_var = T.matrix('inputs')
+        self.context_var = T.matrix('contexts')
+        #self.num_classes = 2
+        #self.target_var = T.ivector('targets')
+        # do regression
+        self.num_classes = 1
+        self.target_var = T.vector('targets')
+
+    def build_pattern(self):
+        raise NotImplemented()
+
+    def build_target_loss(self):
+        phi_output = self.phi.get_output_for(self.input_var)
+        psi_output = self.psi.get_output_for(phi_output).reshape((-1,))
+        self.target_loss = lasagne.objectives.squared_error(psi_output, self.target_var).mean()
+
+
+# ------------------------------------------------------------------------------
+
+class TestDirectPattern(TestBase):
     """
       Test the direct pattern with linear models
     """
-    
 
     def setup(self):
+        self.C = np.array( [[0,1,2,3,4], ] ).T
+        self.m = self.C.shape[1]
+    
         super(TestDirectPattern, self).setup()
-
-        self.init_variables()
-        self.build_pattern()
-        
-    def init_variables(self):
-        self.input_var = T.matrix('inputs')
-        #self.target_var = T.ivector('targets')
-        # do regression
-        self.target_var = T.vector('targets')
-        self.context_var = T.matrix('contexts')
-
-        self.num_classes = 1
-
+    
     def build_pattern(self):
-
         self.input_layer = lasagne.layers.InputLayer(shape=(None, self.n),
                                             input_var=self.input_var)
         self.phi = build_linear_simple( self.input_layer, self.m, name="phi")
@@ -64,29 +76,28 @@ class TestDirectPattern(TestDirectData):
             #nonlinearity=lasagne.nonlinearities.softmax,
             nonlinearity=None,
             name="psi")
+            
+        self.build_target_loss()
     
-        phi_output = self.phi.get_output_for(self.input_var)
-        psi_output = self.psi.get_output_for(phi_output).reshape((-1,))
+#        phi_output = self.phi.get_output_for(self.input_var)
+#        psi_output = self.psi.get_output_for(phi_output).reshape((-1,))
         
         self.pattern = concarne.patterns.DirectPattern(phi=self.phi, psi=self.psi, 
                                              target_var=self.target_var, 
                                              context_var=self.context_var,
-                                             target_loss=lasagne.objectives.squared_error(
-                                                             psi_output,
-                                                             self.target_var).mean()
+                                             target_loss=self.target_loss #lasagne.objectives.squared_error(
+#                                                             psi_output,
+#                                                             self.target_var).mean()
                                              )
 
-        
-
     def test_pattern_output(self):
-
         assert (self.phi.W.get_value(borrow=True).shape == (2,1))
         self.phi.W.set_value ( np.array([[1,0]]).T )
         assert (self.phi.W.get_value(borrow=True).shape == (2,1))
 
-        #assert (psi.W.get_value(borrow=True).shape == (2,1))
         assert (self.psi.W.get_value(borrow=True).shape == (1,1))
         self.psi.W.set_value ( np.array([[1,]]) )
+        assert (self.psi.W.get_value(borrow=True).shape == (1,1))
         
         test_prediction = lasagne.layers.get_output(self.pattern, deterministic=True)
         test_fn = theano.function([self.input_var], test_prediction)
@@ -97,9 +108,7 @@ class TestDirectPattern(TestDirectData):
 
 
     def test_pattern_training_loss_and_grads(self):
-        
-        loss_weights = {'target_weight':0.9, 'context_weight':0.1}
-        loss = self.pattern.training_loss(**loss_weights).mean()
+        loss = self.pattern.training_loss(**self.loss_weights).mean()
         train_fn_inputs = [self.input_var, self.target_var, self.context_var]
         
         train_fn = theano.function(train_fn_inputs, loss)
@@ -110,7 +119,7 @@ class TestDirectPattern(TestDirectData):
         phi_params = lasagne.layers.get_all_params(self.phi, trainable=True)
         psi_params = lasagne.layers.get_all_params(self.psi, trainable=True)
 
-        assert (len (params) == len(set (phi_params+(psi_params))))
+        assert (len (params) == len(set (phi_params+psi_params)))
         
         lst = T.grad(loss, params)
         assert (len (lst) == len (params))
@@ -118,54 +127,84 @@ class TestDirectPattern(TestDirectData):
 
 # ------------------------------------------------------------------------------
 
-class TestMultiViewPattern(TestDirectData):
+class TestMultiViewPattern(TestBase):
     """
       Test the multi view pattern with linear models
     """
 
     def setup(self):
-        super(TestMultiViewPattern, self).setup()
         # embedded C
         self.C = np.array( [[0,1,2,3,4], [2,2,2,2,2]] ).T
+        
+        self.m = self.C.shape[1]
+        self.d = 1
+
+        super(TestMultiViewPattern, self).setup()
+        
+    def build_pattern(self):
+        self.input_layer = lasagne.layers.InputLayer(shape=(None, self.n),
+                                            input_var=self.input_var)
+        self.context_input_layer = lasagne.layers.InputLayer(shape=(None, self.m),
+                                            input_var=self.context_var)
+        self.phi = build_linear_simple( self.input_layer, self.d, name="phi")
+        self.psi = build_linear_simple( self.phi, self.num_classes, 
+            nonlinearity=None, name="psi")
+        self.beta = build_linear_simple( self.context_input_layer, self.d, name="beta")
+            
+        self.build_target_loss()
+        
+        self.pattern = concarne.patterns.MultiViewPattern(phi=self.phi, psi=self.psi, 
+                                             beta=self.beta,
+                                             target_var=self.target_var, 
+                                             context_var=self.context_var,
+                                             target_loss=self.target_loss,
+                                             )
+
+    def test_pattern_output(self):
+
+        print (self.phi.W.get_value(borrow=True))
+        assert (self.phi.W.get_value(borrow=True).shape == (self.n,self.d))
+        self.phi.W.set_value ( np.array([[1,0]]).T )
+        assert (self.phi.W.get_value(borrow=True).shape == (self.n,self.d))
+
+        assert (self.psi.W.get_value(borrow=True).shape == (self.d,self.num_classes))
+        self.psi.W.set_value ( np.array([[1,]]) )
+        assert (self.psi.W.get_value(borrow=True).shape == (self.d,self.num_classes))
+
+        assert (self.beta.W.get_value(borrow=True).shape == (self.m, self.d))
+        self.beta.W.set_value ( np.array([[1,0]]).T )
+        assert (self.beta.W.get_value(borrow=True).shape == (self.m, self.d))
+        
+        test_prediction = lasagne.layers.get_output(self.pattern, deterministic=True)
+        test_fn = theano.function([self.input_var], test_prediction)
+        X_hat = test_fn(self.X)
+        assert ( np.all(X_hat == self.S) )
+
+        beta_prediction = lasagne.layers.get_output(self.beta, deterministic=True)
+        beta_fn = theano.function([self.context_var], beta_prediction)
+        C_hat = beta_fn(self.C)
+        assert ( np.all(C_hat == self.S) )
 
 
-#    def test_pattern(self):
-#        input_var = T.matrix('inputs')
-#        target_var = T.ivector('targets')
-#        context_var = T.matrix('contexts')
-#        
-#        input_layer = lasagne.layers.InputLayer(shape=(None, self.n),
-#                                            input_var=input_var)
-#        phi = build_linear_simple( input_layer, self.m, name="phi")
-#        psi = build_linear_simple( phi, self.num_classes, 
-#            #nonlinearity=lasagne.nonlinearities.softmax,
-#            nonlinearity=None,
-#            name="psi")
-#    
-#        dp = concarne.patterns.DirectPattern(phi=phi, psi=psi, 
-#                                             target_var=target_var, 
-#                                             context_var=context_var,
-#                                             target_loss=lasagne.objectives.squared_error(
-#                                                             lasagne.layers.get_output(phi, input_var),
-#                                                             #phi.get_output_for(input_var), 
-#                                                             context_var)
-#                                             )
-#                                      
-#        assert (phi.W.get_value(borrow=True).shape == (2,1))
-#        phi.W.set_value ( np.array([[1,0]]).T )
-#        assert (phi.W.get_value(borrow=True).shape == (2,1))
-#
-#        #assert (psi.W.get_value(borrow=True).shape == (2,1))
-#        assert (psi.W.get_value(borrow=True).shape == (1,1))
-#        psi.W.set_value ( np.array([[1,]]) )
-#        
-#        test_prediction = lasagne.layers.get_output(dp, deterministic=True)
-#        test_fn = theano.function([input_var], test_prediction)
-#        
-#        X_hat = test_fn(self.X)
-#    
-#        assert ( np.all(self.C == X_hat) )
+    def test_pattern_training_loss_and_grads(self):
+        loss = self.pattern.training_loss(**self.loss_weights).mean()
+        train_fn_inputs = [self.input_var, self.target_var, self.context_var]
+        
+        train_fn = theano.function(train_fn_inputs, loss)
+        assert (train_fn(self.X, self.Y, self.C) > 0)
 
+        params = lasagne.layers.get_all_params(self.pattern, trainable=True)
+        
+        phi_params = lasagne.layers.get_all_params(self.phi, trainable=True)
+        psi_params = lasagne.layers.get_all_params(self.psi, trainable=True)
+        beta_params = lasagne.layers.get_all_params(self.beta, trainable=True)
+
+        assert (len (params) == len(set (phi_params+psi_params+beta_params)))
+        
+        lst = T.grad(loss, params)
+        assert (len (lst) == len (params))
+
+# ------------------------------------------------------------------------------
     
 if __name__ == "__main__":
     td = TestDirectPattern()
