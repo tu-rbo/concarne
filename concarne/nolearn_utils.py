0a317c0a1b7aa175ba2289a5247415f486b98e27
Sebastian Höfer <sebastian.hoefer@tu-berlin.de>
2016-04-23 00:46:49 +0200

    added nolearn utils to use nolearn.lasagne functionality

---
+++ concarne/nolearn_utils.py
@@ -0,0 +1,49 @@
+"""
+A set of helpers to use nolearn.lasagne functionality.
+
+The most important function is `pattern2neuralnet` which duck-types any Pattern object
+such that it behaves similarly to a nolearn.lasagne.NeuralNet object.
+
+Supported `handlers`:
+- PrintLayerInfo
+
+Supported `visualize` methods:
+- plot_conv_weights
+- plot_conv_activity
+
+"""
+
+import nolearn.lasagne
+from nolearn.lasagne.base import Layers
+
+import nolearn.lasagne.handlers
+import nolearn.lasagne.visualize
+
+PrintLayerInfo = nolearn.lasagne.handlers.PrintLayerInfo
+plot_conv_weights = nolearn.lasagne.visualize.plot_conv_weights
+plot_conv_activity = nolearn.lasagne.visualize.plot_conv_activity
+plot_occlusion = nolearn.lasagne.visualize.plot_occlusion
+
+def pattern2neuralnet(pattern, verbose=0):
+    pattern.layers = []
+    pattern.layers_ = Layers()
+    pattern.verbose = verbose
+    
+    for fn_name in ['phi', 'psi', 'beta']:
+        if fn_name not in pattern.__dict__ or pattern.__dict__[fn_name] is None:
+            continue
+            
+        fn = pattern.__dict__[fn_name]
+        for i,l in enumerate(pattern._get_all_function_layers(fn)):
+            name = l.name
+            dct = l.__dict__
+            if name is None:
+                name = ""
+            prefix = "%s%d" % (fn_name, i)
+            if prefix not in name:
+                name = prefix + ("_" + name if name != "" else name)
+            dct['name'] = name
+            pattern.layers_[name] = l    
+            pattern.layers.append (( l.__class__, dct))
+            
+
